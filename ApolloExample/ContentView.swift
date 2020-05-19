0491501e145371f05e5f6792b74be545a142ecad
Ilario Salatino <ilahomesick@gmail.com>
2020-05-19 07:39:01 +0200

    Initial Commit

---
+++ ApolloExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApolloExample
+//
+//  Created by ilario salatino on 19.05.20.
+//  Copyright © 2020 ilario salatino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
